e17936828b36e0c6dee3f5b2f48b6be6f4e21a35
CalArcher <p.calarch@gmail.com>
2022-05-05 20:31:21 -0400

    removed google api key

--- webpage/main.js
+++ webpage/main.js
@@ -1,8 +1,9 @@
 
 let mapIframe = document.getElementById('mapIframe')
-let lat = 5
-let long = 6
+let lat = 22
+let long = 70
+
 
 document.querySelector('button').addEventListener('click', () => {
-    mapIframe.src = `https://www.google.com/maps/embed/v1/view?key=AIzaSyDodhFFW3Wz6zw0c9TmzMMSdrsANKgBIEU&center=${lat},${long}&zoom=10&maptype=satellite`
+    mapIframe.src = `https://www.google.com/maps/embed/v1/view?key=${GOOGLE_KEY}&center=${lat},${long}&zoom=10&maptype=satellite`
 })
